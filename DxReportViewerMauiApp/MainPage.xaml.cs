1c4d2f8f5150d216fee063772276c7b5a495a06f
Serge Pilipchuk <112853396+sergepilipchuk@users.noreply.github.com>
2025-12-11 12:42:38 +0400

    Add BlazorWebView to MainPage
    Added BlazorWebView field to MainPage class.

--- DxReportViewerMauiApp/MainPage.xaml.cs
+++ DxReportViewerMauiApp/MainPage.xaml.cs
@@ -1,7 +1,11 @@
-﻿namespace DxReportViewerMauiApp {
+using Microsoft.AspNetCore.Components.WebView.Maui;
+
+namespace DxReportViewerMauiApp {
     public partial class MainPage : ContentPage {
+        private BlazorWebView blazorWebView;
         public MainPage() {
             InitializeComponent();
         }
     }
 }
+
