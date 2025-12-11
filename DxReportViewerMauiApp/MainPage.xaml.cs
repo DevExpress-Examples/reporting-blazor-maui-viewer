6435751808502d7b7fecd1abcd97e1abd66ed2ab
Serge Pilipchuk <112853396+sergepilipchuk@users.noreply.github.com>
2025-12-11 13:01:10 +0400

    Remove unused BlazorWebView reference from MainPage

--- DxReportViewerMauiApp/MainPage.xaml.cs
+++ DxReportViewerMauiApp/MainPage.xaml.cs
@@ -1,13 +1,12 @@
-using Microsoft.AspNetCore.Components.WebView.Maui;
 using Microsoft.Maui.Controls;
 
 namespace DxReportViewerMauiApp {
     public partial class MainPage : ContentPage {
-        private BlazorWebView blazorWebView;
         public MainPage() {
             InitializeComponent();
         }
     }
 }
+
 
 
